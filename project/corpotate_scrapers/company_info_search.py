57ccf8de9c618688894bd4a15e75daa33c886939
Your Name <you@example.com>
2024-03-28 18:34:35 +0300

    start refactoring SOLID

--- project/corpotate_scrapers/company_info_search.py
+++ project/corpotate_scrapers/company_info_search.py
@@ -1,6 +1,8 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+from project.helpers import operate
+import logging
 
 
 class Company:
@@ -25,23 +27,11 @@ class Company:
         def look(info):
             return self.page.find(string=re.compile(f".*\{info}.*")).parent.next_sibling.next_sibling.text
 
-        def operate(operation, info=None):
-            try:
-                if self.page:
-                    if info:
-                        result = operation(info)
-                        return result
-                    return operation()
-                return None
-            except (AttributeError, TypeError) as e:
-                print(e)
-                return None
-
         company_info = {"inn": self.inn,
                         "website": operate(look, "Веб-сайт"),
                         "organization": operate(lambda: self.page.find("h1", class_="organization-name").text),
                         "ogrn": operate(lambda: self.page.find(id="copy-ogrn").text),
-                        "registration_date": Company.format_date(operate(look, "Дата регистрации")),
+                        "registration_date": DateFormat(operate(look, "Дата регистрации")),
                         "sphere": operate(look, "Вид деятельности"),
                         "address": operate(look, "Юридический адрес"),
                         "workers_number": operate(lambda info: look(info).split()[0],
@@ -49,8 +39,16 @@ class Company:
                         "ceo": operate(lambda: self.page.find("img", width="92")["title"])}
         return company_info
 
+
+class DateFormat:
+    def __init__(self, date):
+        self.date = date
+
+    def __repr__(self):
+        return self.__format_date(self.date)
+
     @staticmethod
-    def format_date(date: str):
+    def __format_date(date: str):
         try:
             months = {
                 "января": "01",
@@ -64,17 +62,20 @@ class Company:
                 "сентября": "09",
                 "октября": "10",
                 "ноября": "11",
-                "декабря": "11"}
-            day, month, year, etc = date.split()
+                "декабря": "12"}
+            date = date.split()
+            day = date[0]
+            month = date[1]
+            year = date[2]
             month = months[month]
             return "-".join([i.strip() for i in [day, month, year]])
-        except (ValueError, KeyError, AttributeError) as e:
-            print(e)
+        except (ValueError, KeyError, AttributeError, IndexError) as e:
+            logging.warning(e)
             return None
 
 
 if __name__ == "__main__":
-    print(Company.format_date("17 феврали 2011"))
+    print(DateFormat("17 февраля 2011 фвыафыва"))
     inn = 4704041900
     AGV = Company(inn)
     print(AGV.get_full_info())
