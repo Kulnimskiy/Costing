89601ed1e98e338eed84c9ae6b6c171cd73298b8
Sergey <sergeish2012@yandex.ru>
2024-04-16 20:52:19 +0300

    all scrapers work well

--- project/clients_scrapers/7728353396/7709847093.py
+++ project/clients_scrapers/7728353396/7709847093.py
@@ -13,6 +13,19 @@ class Kkdmlhkdmg:
     SEARCH_URL = "https://dentex.ru/search/?q={}&area=everywhere&s="
 
     @staticmethod
+    def convert_price(price):
+        """converts the price into the right currency and turns it into an int """
+        price_raw = PriceManager(price)
+        if "€" in price.lower():
+            return price_raw.convert_to_rub(currency="EUR")
+        elif "$" in price.lower():
+            return price_raw.convert_to_rub(currency="USD")
+        elif "руб" not in price.lower():  # in case there are other currencies
+            logging.warning(f"THE PRICE {price} COULD NOT BEEN CONVERTED TO RUB")
+            return None
+        return price_raw.check()
+
+    @staticmethod
     async def search_relevant_items(item: str, session: aiohttp.ClientSession) -> Union[list, None]:
         """Get a list of relevant items from the competitor's website"""
 
@@ -35,8 +48,8 @@ class Kkdmlhkdmg:
             # there often are old and new price. Get the new one
             price_raw = str(item_in_catalog.find("div", class_="price").text)
             price_raw = price_raw.strip().split("\n")[0]
-            # change the type of the price to an int. None if there are no digits.
-            return PriceManager(price_raw).check()
+            # change the type of the price to an int. None if there are no digits
+            return Kkdmlhkdmg.convert_price(price_raw)
 
         @OperationalTools.operate
         def get_link(item_in_catalog: BeautifulSoup):
@@ -94,21 +107,21 @@ class Kkdmlhkdmg:
     async def get_item_info(link: str, session: aiohttp.ClientSession) -> Union[dict, None]:
         """ Get the name, price and link of a competitor's product via the provided web page """
 
-        check_cls_tag = "detail-product-page in-basket"
+        check_cls_tag = "item-head"
 
         @OperationalTools.operate
         def get_check(document: BeautifulSoup):
             """ checks if the page loads correctly and that is the right page to get the info from """
-            return document.find(id="catalog-products").find_all(class_=check_cls_tag)
+            return document.find(class_="item-head")
 
         @OperationalTools.operate
         def get_name(document):
-            return document.find(class_="detail-product-page in-basket").find("h1").get_text()
+            return document.find(class_="item-head").find("h1").get_text()
 
         @OperationalTools.operate
         def get_price(document: BeautifulSoup):
-            price_raw = document.find(class_="dpp-price_data__price").find(class_="current-price").text
-            return PriceManager(price_raw).check()
+            price_raw = document.find(class_="item-head").find(class_="price").find(class_="currency").text
+            return Kkdmlhkdmg.convert_price(price_raw)
 
         # The main algorithm is implemented here
         if Kkdmlhkdmg.BASE_URL not in link:
@@ -157,127 +170,4 @@ def run_test(item, link):
 
 
 if __name__ == '__main__':
-    run_test("стул", "https://dentikom.ru/catalog/dental-chairs/physio-one/")
-
-import asyncio
-import logging
-import aiohttp
-from typing import Union
-from bs4 import BeautifulSoup
-from project.wreckage.helpers import operate, convert_to_rub, get_web, check_price, get_link
-from project.credentials import TIMEOUT
-
-
-class Kkdmlhkdmg:
-    INN = 7709847093
-    BASE_URL = "https://dentex.ru"
-    SEARCH_URL = "https://dentex.ru/search/?q={}&area=everywhere&s="
-
-    @staticmethod
-    async def search_relevant_items(item: str, session: aiohttp.ClientSession) -> Union[list, None]:
-        """Get a list of relevant items from the competitor's website"""
-        try:
-            logging.warning(f"Sent to {Kkdmlhkdmg.BASE_URL}")
-            req = await session.get(Kkdmlhkdmg.SEARCH_URL.format(item))
-            res = await req.text()
-            doc = BeautifulSoup(res, "html.parser")
-            logging.warning(f"Got from {Kkdmlhkdmg.BASE_URL}")
-
-            # check if the page is loaded correctly. If not, try getting it through the browser
-            check = operate(lambda: doc.find("div", class_="short-search"))
-            if not check:
-                logging.warning(f"BROWSER IS WORKING IN {Kkdmlhkdmg.SEARCH_URL.format(item)}")
-                res = get_web(Kkdmlhkdmg.SEARCH_URL.format(item), "short-search", TIMEOUT)
-                doc = BeautifulSoup(res, "html.parser")
-
-            items_found = operate(lambda: doc.find("div", class_="short-search").find_all("div", class_="item-mini"))
-            if not items_found:
-                logging.warning(f"There are no items in {Kkdmlhkdmg.BASE_URL}")
-                return None
-
-            items_lst = []
-            for item in items_found:
-                # give it the path to the name
-                name = operate(lambda: str(item.find("a", class_="cover-link")["title"]))
-                if not name:
-                    logging.warning(f"There is no name. Item has been skipped")
-                    continue
-
-                # links are provided with no base url
-                link = get_link(operate(lambda: Kkdmlhkdmg.BASE_URL + str(item.find("a", class_="cover-link")["href"])))
-                if not link:
-                    logging.warning(f"LINK FOR THE ITEM HASN'T BEEN FOUND. ITEM NAME: " + name)
-                    continue
-
-                # there often are old and new price. Get the new one
-                price = operate(lambda: str(item.find("div", class_="price").text))
-
-                # change the type of the price to an int. None if there are no digits.
-                price = Kkdmlhkdmg.price_format(price)
-
-                items_lst.append({"name": name, "price": price, "url": link})
-            return items_lst
-        except Exception as error:
-            logging.warning(f"ERROR: {error} IN: {Kkdmlhkdmg.BASE_URL}")
-            return None
-
-    @staticmethod
-    async def get_item_info(link: str, session: aiohttp.ClientSession) -> Union[dict, None]:
-        """ Get the name, price and link of a competitor's product via the provided web page """
-        if Kkdmlhkdmg.BASE_URL not in link:
-            logging.warning(f"WRONG LINK PROVIDED FOR {Kkdmlhkdmg.BASE_URL}")
-            return None
-        try:
-            logging.warning(f"Sent to {Kkdmlhkdmg.BASE_URL}")
-            req = await session.get(link)
-            doc = await req.text()
-            logging.warning(f"Got from {Kkdmlhkdmg.BASE_URL}")
-            doc = BeautifulSoup(doc, "html.parser")
-
-            # check if the page is loaded correctly. If not, try getting it through the browser
-            check = doc.find(class_="item-head")
-            if not check:
-                doc = get_web(link, "recover-password-cabinet-1c")
-                doc = BeautifulSoup(doc, "html.parser")
-
-            page = doc.find(class_="item-head")
-
-            name = operate(lambda: page.find("h1").get_text())
-            if not name:
-                logging.warning(f"There is no items in {link}")
-                return None
-
-            price = operate(lambda: page.find(class_="price").find(class_="currency").text)
-            price = Kkdmlhkdmg.price_format(price)
-            return {"name": name, "price": price, "url": link}
-        except Exception as error:
-            logging.warning(f"ERROR: {error} IN: {Kkdmlhkdmg.BASE_URL}")
-            return None
-
-    @staticmethod
-    def price_format(price):
-        """converts the price into the right currency and turnes it into an int"""
-        price_int = check_price(price)
-        if "€" in price.lower():
-            price_int = convert_to_rub(price_int, "EUR")
-        elif "$" in price.lower():
-            price_int = convert_to_rub(price_int, "USD")
-        elif "руб" not in price.lower():  # in case there are other currencies
-            price_int = None
-        return price_int
-
-
-async def test_search(item):
-    async with aiohttp.ClientSession() as session:
-        result = await Kkdmlhkdmg.search_relevant_items(item, session)  # link[1] is the url of the item
-        return result
-
-
-def run_test(item):
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    results = asyncio.run(test_search(item))
-    print(results)
-
-
-if __name__ == '__main__':
-    run_test("стул")
+    run_test("стул", "https://dentex.ru/catalog/dental-equipment/dental-unit/swident/friend-easy-swident/")
--- project/clients_scrapers/7728353396/7801334382.py
+++ project/clients_scrapers/7728353396/7801334382.py
@@ -1,9 +1,10 @@
 import asyncio
 import logging
 import aiohttp
+from typing import Union
 from bs4 import BeautifulSoup
-from project.wreckage.helpers import operate, get_web, check_price
-from project.credentials import TIMEOUT
+from project.helpers import OperationalTools, Browser
+from project.managers import UrlManager, PriceManager
 
 
 class Kldegghglf:
@@ -32,48 +33,70 @@ class Kldegghglf:
             return None
 
     @staticmethod
-    async def get_item_info(link: str, session):
+    async def get_item_info(link: str, session: aiohttp.ClientSession) -> Union[dict, None]:
+        """ Get the name, price and link of a competitor's product via the provided web page """
+
+        check_cls_tag = "h2 text-center content-title content-title-copy-parent"
+
+        @OperationalTools.operate
+        def get_check(document: BeautifulSoup):
+            """ checks if the page loads correctly and that is the right page to get the info from """
+            return document.find(class_=check_cls_tag)
+
+        @OperationalTools.operate
+        def get_name(document):
+            return document.find(class_="h2 text-center content-title content-title-copy-parent").find("h1").get_text()
+
+        @OperationalTools.operate
+        def get_price(document: BeautifulSoup):
+            price_raw = document.find(class_="autocalc-product-price").get_text()
+            return PriceManager(price_raw).check()
+
+        # The main algorithm is implemented here
         if Kldegghglf.BASE_URL not in link:
             logging.warning(f"WRONG LINK PROVIDED FOR {Kldegghglf.BASE_URL}")
             return None
         try:
             logging.warning(f"Sent to {Kldegghglf.BASE_URL}")
-            req = await session.get(link)
-            doc = await req.text()
+            req = await session.get(link, ssl=False)
+            res = await req.text()
             logging.warning(f"Got from {Kldegghglf.BASE_URL}")
-            doc = BeautifulSoup(doc, "html.parser")
+            doc = BeautifulSoup(res, "html.parser")
 
             # check if the page is loaded correctly. If not, try getting it through the browser
-            check = operate(lambda: doc.find(class_="h2 text-center content-title content-title-copy-parent"))
+            check = get_check(doc)
             if not check:
-                res = get_web(link, "h2 text-center content-title content-title-copy-parent", TIMEOUT)
+                res = Browser(link).get_page(cls_wait_tag=check_cls_tag)
                 doc = BeautifulSoup(res, "html.parser")
 
-            name = operate(lambda: doc.find(class_="h2 text-center content-title content-title-copy-parent").find(
-                "h1").get_text())
+            name = get_name(doc)
+
             if not name:
-                logging.warning(f"There is no items in {link}")
+                logging.warning(f"THERE ARE NO ITEMS IN {link}")
                 return None
 
-            price = operate(lambda: doc.find(class_="autocalc-product-price").get_text())
-            price = check_price(price)
+            price = get_price(doc)
+            if not price:
+                logging.warning(f"THERE IS NO PRICE IN {link}")
+
             return {"name": name, "price": price, "url": link}
         except Exception as error:
-            print(error, Kldegghglf.BASE_URL)
+            logging.warning(f"ERROR: {error} IN: {Kldegghglf.BASE_URL}")
             return None
 
 
-async def test_search(item):
+async def test_search(item, link=None):
     async with aiohttp.ClientSession() as session:
         result = await Kldegghglf.search_relevant_items(item, session)  # link[1] is the url of the item
-        return result
+        result_link = await Kldegghglf.get_item_info(link, session) if link else None
+        return result, result_link
 
 
-def run_test(item):
+def run_test(item, link):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    results = asyncio.run(test_search(item))
+    results = asyncio.run(test_search(item, link))
     print(results)
 
 
 if __name__ == '__main__':
-    run_test("стул")
+    run_test("стул", "https://stomshop.pro/averon-stul-2-1-folk-new")
